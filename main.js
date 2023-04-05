701e75675bb5d35f8c9130cb5fa5cc320333e5ce
Asama Qureshi <asama.qureshi@gmail.com>
2023-04-05 22:58:13 +1000

    include built files, remove dist folder. built files in docs folder

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static("dist"));
+app.use(express.static("docs"));
 
 app.listen(3000, () => {
   console.log("listening on 3000");
