2fdc390991a473a6f952ac4dd56b5bcf7dde33c4
Asama Qureshi <asama.qureshi@gmail.com>
2023-04-05 19:02:28 +1000

    parcel config to build straight into docs, for use with gh-pages

--- dist/utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.repeat = void 0;
-const repeat = (n, pattern) => {
-    return [...Array(n)].reduce((sum) => sum.concat(pattern), []);
-};
-exports.repeat = repeat;
-//# sourceMappingURL=utils.js.map
